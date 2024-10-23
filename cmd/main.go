6209592996b9aca88cd8154b437d80ddeb0137b8
Dimitri Kaiser <38867887+Skryvvara@users.noreply.github.com>
2024-10-23 20:02:29 +0200

    feat: add utilities to systray (#30)

--- cmd/main.go
+++ cmd/main.go
@@ -6,11 +6,16 @@ import (
 	"github.com/getlantern/systray"
 	"github.com/skryvvara/focusframe/config"
 	"github.com/skryvvara/focusframe/input"
+	"github.com/skryvvara/focusframe/internal/browser"
 	"github.com/skryvvara/focusframe/window"
 	"log"
 )
 
 var Version = "vX.Y.Z" // this is set during build time
+
+const (
+	REPO_URL = "https://github.com/skryvvara/focusframe"
+)
 
 //go:embed monitor.ico
 var iconFS embed.FS
@@ -35,24 +40,43 @@ func onReady() {
 	systray.SetTitle("FocusFrame")
 	systray.SetTooltip(fmt.Sprintf("FocusFrame Version: %s", Version))
 
-	systray.AddSeparator()
-
-	mRunOnStartupCh := systray.AddMenuItem("Run on Startup", "Run the application when starting the PC (Not implemented)")
+	mManageApplications := systray.AddMenuItem("Manage Applications", "Manage Applications")
 
 	systray.AddSeparator()
 
-	mQuitCh := systray.AddMenuItem("Quit", "Quit the whole app")
+	mWiki := systray.AddMenuItem("Open Wiki", "Open Wiki")
+	mForum := systray.AddMenuItem("Open Forum", "Open Forum")
+	mGithub := systray.AddMenuItem("Open Github", "Open Github repository")
+	mRunOnStartup := systray.AddMenuItem("Run on Startup", "Run the application when starting the PC (Not implemented)")
+
+	systray.AddSeparator()
+
+	mQuit := systray.AddMenuItem("Quit", "Quit the whole app")
 
 	for {
 		select {
-		case <-mRunOnStartupCh.ClickedCh:
-			if mRunOnStartupCh.Checked() {
-				mRunOnStartupCh.Uncheck()
-			} else {
-				mRunOnStartupCh.Check()
+		case <-mManageApplications.ClickedCh:
+			log.Println("Not implemented")
+		case <-mWiki.ClickedCh:
+			if err := browser.OpenURL(REPO_URL + "/wiki"); err != nil {
+				log.Println(err)
 			}
-		case <-mQuitCh.ClickedCh:
-			log.Println("Requesting Exit")
+		case <-mForum.ClickedCh:
+			if err := browser.OpenURL(REPO_URL + "/discussions"); err != nil {
+				log.Println(err)
+			}
+		case <-mGithub.ClickedCh:
+			if err := browser.OpenURL(REPO_URL); err != nil {
+				log.Println(err)
+			}
+		case <-mRunOnStartup.ClickedCh:
+			if mRunOnStartup.Checked() {
+				mRunOnStartup.Uncheck()
+			} else {
+				mRunOnStartup.Check()
+			}
+		case <-mQuit.ClickedCh:
+			fmt.Println("Requesting Exit")
 			systray.Quit()
 		}
 	}
