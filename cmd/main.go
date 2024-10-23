bf773f0a0137cae4c7770f585c5935f0e99767bd
Dimitri Kaiser <38867887+Skryvvara@users.noreply.github.com>
2024-10-23 19:56:49 +0200

    feat: add icon for executable and tray (#29)

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"embed"
 	"fmt"
 	"github.com/getlantern/systray"
-	"github.com/getlantern/systray/example/icon"
 	"github.com/skryvvara/focusframe/config"
 	"github.com/skryvvara/focusframe/input"
 	"github.com/skryvvara/focusframe/window"
+	"log"
 )
 
 var Version = "vX.Y.Z" // this is set during build time
+
+//go:embed monitor.ico
+var iconFS embed.FS
 
 func main() {
 	config.Initialize()
@@ -22,7 +26,12 @@ func main() {
 
 // onReady setup systray
 func onReady() {
-	systray.SetIcon(icon.Data)
+	iconData, err := iconFS.ReadFile("monitor.ico")
+	if err != nil {
+		log.Fatal("Error reading icon: ", err)
+	}
+
+	systray.SetIcon(iconData)
 	systray.SetTitle("FocusFrame")
 	systray.SetTooltip(fmt.Sprintf("FocusFrame Version: %s", Version))
 
@@ -43,7 +52,7 @@ func onReady() {
 				mRunOnStartupCh.Check()
 			}
 		case <-mQuitCh.ClickedCh:
-			fmt.Println("Requesting Exit")
+			log.Println("Requesting Exit")
 			systray.Quit()
 		}
 	}
