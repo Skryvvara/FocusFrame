b56e3b3b105437384e529909f6ea18a06b98eafe
Dimitri Kaiser <darkyne.gaming@outlook.de>
2024-12-23 19:29:22 +0100

    refactor: refactor process package

--- process/process.go
+++ process/process.go
@@ -2,11 +2,12 @@ package process
 
 import (
 	"fmt"
-	"github.com/StackExchange/wmi"
-	"golang.org/x/sys/windows"
 	"path/filepath"
 	"syscall"
 	"unsafe"
+
+	"github.com/StackExchange/wmi"
+	"golang.org/x/sys/windows"
 )
 
 var (
@@ -31,30 +32,41 @@ type Win32_Process struct {
 	Name      string
 }
 
-func GetProcessIDByExecutable(executable string) uint32 {
+// GetProcessIDByExecutable tries to find the PID (Process ID) of a process with
+// the given executable.
+//
+// Returns either the PID as a uint32 or an error if no PID could be found.
+func GetProcessIDByExecutable(executable string) (uint32, error) {
 	var processes []Win32_Process
 	query := fmt.Sprintf("SELECT ProcessID, Name FROM Win32_Process WHERE Name='%s'", executable)
 	err := wmi.Query(query, &processes)
 	if err != nil {
-		fmt.Println("Error querying WMI:", err)
-		return 0
+		return 0, fmt.Errorf("error querying WMI: %v", err)
 	}
 
 	if len(processes) > 0 {
-		return processes[0].ProcessID
+		return processes[0].ProcessID, nil
 	}
-	return 0
+	return 0, nil
 }
 
-func GetProcessIDFromWindow(hwnd syscall.Handle) (uint32, error) {
+// GetProcessIDFromWindow tries to find the PID (Process ID) of a process with
+// the given window handle.
+//
+// Returns either the PID as a uint32 or an error if no PID could be found.
+func GetProcessIDFromWindow(hWnd uintptr) (uint32, error) {
 	var pid uint32
-	_, _, err := procGetWindowThreadProcessId.Call(uintptr(hwnd), uintptr(unsafe.Pointer(&pid)))
+	_, _, err := procGetWindowThreadProcessId.Call(hWnd, uintptr(unsafe.Pointer(&pid)))
 	if pid == 0 {
 		return 0, fmt.Errorf("failed to get PID: %v", err)
 	}
 	return pid, nil
 }
 
+// GetFullExecutableFromPID tries to get the full path of the executable of a process
+// with the given PID (Process ID). E.g. C:/Applications/Program.exe
+//
+// Returns either the Executable as a string or an error if the executable could not be found.
 func GetFullExecutableFromPID(pid uint32) (string, error) {
 	hProcess, _, _ := procOpenProcess.Call(PROCESS_QUERY_INFORMATION|PROCESS_VM_READ, 0, uintptr(pid))
 	if hProcess == 0 {
@@ -76,28 +88,30 @@ func GetFullExecutableFromPID(pid uint32) (string, error) {
 	return syscall.UTF16ToString(exePath), nil
 }
 
+// GetExecutableFromPID functions like GetFullExectuableFromPID but returns only the executable's name.
+// E.g Program.exe
+//
+// For more information, see GetFullExecutableFromPID.
+//
+// Returns either the Executable as a string or an error if the executable could not be found.
 func GetExecutableFromPID(pid uint32) (string, error) {
-	hProcess, _, _ := procOpenProcess.Call(PROCESS_QUERY_INFORMATION|PROCESS_VM_READ, 0, uintptr(pid))
-	if hProcess == 0 {
-		return "", fmt.Errorf("failed to open process: %v", syscall.GetLastError())
-	}
-	defer procCloseHandle.Call(hProcess)
-
-	exePath := make([]uint16, syscall.MAX_PATH)
-	_, _, err := procGetModuleFileNameEx.Call(hProcess, 0, uintptr(unsafe.Pointer(&exePath[0])), syscall.MAX_PATH)
-	if err != syscall.Errno(0) {
-		return "", fmt.Errorf("failed to get module file name: %v", err)
+	fullPath, err := GetFullExecutableFromPID(pid)
+	if err != nil {
+		return "", err
 	}
 
-	fullPath := syscall.UTF16ToString(exePath)
 	// Extract the base name from the full path (e.g., "bg3.exe" from "F:\path\to\bg3.exe")
 	baseName := filepath.Base(fullPath)
 
 	return baseName, nil
 }
 
-func GetExecutableFromHandle(hwnd syscall.Handle) (string, error) {
-	pid, err := GetProcessIDFromWindow(hwnd)
+// GetExecutableFromHandle tries to get the (short) executable name of a window
+// given the window handle.
+//
+// Returns either the Executable as a string or an error if the executable could not be found.
+func GetExecutableFromHandle(hWnd uintptr) (string, error) {
+	pid, err := GetProcessIDFromWindow(hWnd)
 	if err != nil {
 		return "", err
 	}
